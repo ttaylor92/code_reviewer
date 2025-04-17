8974820d77438f5ddf2c919a709f505462720765
Tyrone Taylor <28680107+ttaylor92@users.noreply.github.com>
2025-04-17 16:52:48 -0500

    Update page controller test to assert "Page Not Found" message on GET request to root path

--- test/code_reviewer_web/controllers/page_controller_test.exs
+++ test/code_reviewer_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CodeReviewerWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Page Not Found"
   end
 end
